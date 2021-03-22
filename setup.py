8986c997dcff4e978f66de66472edc38fd8cb245
Ahmed Abdelmoneim <ahmedh.abdelmoneim@gmail.com>
2021-03-22 13:27:11 -0700

    update req.txt

--- setup.py
+++ setup.py
@@ -27,22 +27,4 @@ setuptools.setup(
     entry_points={
         "console_scripts": ['sgrnable = optimal_guide_finder.guide_finder:main']
     },
-    install_requires=[
-        'matplotlib==3.2.1',
-        'pandas==0.24.2',
-        'XlsxWriter==1.1.5',
-        'numpy==1.16.2',
-        'scipy==1.4.1',
-        'biopython==1.76',
-        'GPy==1.9.9',
-        'ipython==7.10.1',
-        'azimuth==2.0',
-        'elevation==1.0.6',
-        'hyperopt==0.2.2',
-        'ipdb==0.12.3',
-        'mkl==2019.0',
-        'scikit_learn==0.20.2',
-        'theanets==0.7.3',
-        'numba==0.48.0',
-    ],
 )
